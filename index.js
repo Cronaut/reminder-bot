9ef31c8930168440abc80785ee0f9225a00d92a8
Cronaut <cronadev@gmail.com>
2017-06-16 20:17:35 -0700

    Alright, should work

--- index.js
+++ index.js
@@ -1,3 +1,48 @@
-//Bot authentication
-
 'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('request')
+const app = express()
+
+app.set('port', (process.env.PORT || 5000))
+
+// Process application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// Process application/json
+app.use(bodyParser.json())
+
+// Index route
+app.get('/', function (req, res) {
+    res.send('Sup, it\'s a chat bot.')
+})
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
+        res.send(req.query['hub.challenge'])
+    }
+    res.send('Error, wrong token')
+})
+
+// Spin up the server
+app.listen(app.get('port'), function () {
+    console.log('running on port', app.get('port'))
+})
+
+// Post stuff
+app.post('/webhook/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+        if (event.message && event.message.text) {
+            let text = event.message.text
+            sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
+        }
+    }
+    res.sendStatus(200)
+})
+
+const token = "EAAGII9tDr9EBAFSBYt0j0o4IeRFqZBnmIZB2z2Yh3ZBzZAleQRKIoh31Sbywq2EIcaZCfztYXyeHKkagtlwcYG0CshJaRzqsFGwDvHoZAlYbY1CZCV1Tezx2YfZBdBO6mWyFHYZB9Rl6zXvUAC1nf7qov4sloMigCysSLioz6dgbZASOhRAoQZAvDhZA"
