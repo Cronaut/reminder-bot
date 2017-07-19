237019772efa588e638235e76b2aad855d52592d
Cronaut <cronadev@gmail.com>
2017-07-18 20:10:54 -0700

    Add an entry model, attempt to add an entry to the db

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ const mongoose = require('mongoose');
 
 const config = require('./config/database');
 
+// bring up the Entry model
+let Entry = require('../models/entry');
+
 mongoose.connect(config.database);
 let db = mongoose.connection;
 
@@ -35,49 +38,61 @@ let seedData = [
 
 ];
 
-app.set('port', (process.env.PORT || 5000))
+app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
-    res.send('Sup, it\'s a chat bot.')
-})
+    res.send('Sup, it\'s a chat bot.');
+});
 
 // for Facebook verification
 app.get('/webhook/', function (req, res) {
     if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
+        res.send(req.query['hub.challenge']);
     }
-    res.send('Error, wrong token')
-})
+    res.send('Error, wrong token');
+});
 
 // Spin up the server
 app.listen(app.get('port'), function () {
-    console.log('running on port', app.get('port'))
-})
+    console.log('running on port', app.get('port'));
+});
 
 // Post stuff
 app.post('/webhook/', function (req, res) {
-    let messaging_events = req.body.entry[0].messaging
+    let messaging_events = req.body.entry[0].messaging;
     for (let i = 0; i < messaging_events.length; i++) {
-        let event = req.body.entry[0].messaging[i]
-        let sender = event.sender.id
+        let event = req.body.entry[0].messaging[i];
+        let sender = event.sender.id;
         if (event.message && event.message.text) {
             // parseMessage(event.message.text);
 
-            let text = event.message.text
-            sendTextMessage(sender, "HELLO! I\'M A PRETTY ANGRY CORN.")
+            // add the todo entries to db
+            let entries = new Entry();
+            entries.userid = sender;
+            entries.todos = event.message.text;
+
+            entries.save((err) => {
+                if(err) {
+                    console.log(err);
+                    return;
+                } else {
+                    console.log('The entry has been added.');
+                }
+            });
+            sendTextMessage(sender, "Your todo is:" + event.message.text);
         }
     }
-    res.sendStatus(200)
+    res.sendStatus(200);
 })
 
-const token = process.env.FB_PAGE_ACCESS_TOKEN
+const token = process.env.FB_PAGE_ACCESS_TOKEN;
 
 function sendTextMessage(sender, text) {
     let messageData = { text: text }
@@ -91,11 +106,11 @@ function sendTextMessage(sender, text) {
         }
     }, function (error, response, body) {
         if (error) {
-            console.log('Error sending messages: ', error)
+            console.log('Error sending messages: ', error);
         } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
+            console.log('Error: ', response.body.error);
         }
-    })
+    });
 }
 
 function parseMessage(event) {
@@ -110,10 +125,11 @@ function parseMessage(event) {
     } else {
         throw "The passed object is somehow not a string.";
     }
-
     return false;
-    
 }
+
+/*let users = require('./routes/users');
+app.use('/users', users);*/
 
 /*mongodb.MongoClient.connect(uri, (err, db) => {
       if (err) throw err;
---
+++ models/entry.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+// Entry Schema
+
+const EntrySchema = mongoose.Schema({
+    userid:{
+        type: String,
+        required: true
+    },
+    todos:{
+        type: String,
+        required: true
+    }
+    /*todos:{
+        type: Array,
+        "default" : [],
+        required: true
+    }*/
+});
+
+const Entry = module.exports = mongoose.model('Entry', EntrySchema);
